26ac9ceb3f905c5848d767ace305db7053007571
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-24 11:58:26 -0700

    Add actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,29 @@
-export const setImage = (image) => ({
-  type: 'SET_IMAGE',
+export const setDogImage = image => ({
+  type: 'SET_DOG_IMAGE',
   image
-})
+});
+
+export const setCatImage = image => ({
+  type: 'SET_CAT_IMAGE',
+  image
+});
+
+export const setError = error => ({
+  type: 'SET_ERROR',
+  error
+});
+
+export const setLoading = loading => ({
+  type: 'SET_LOADING',
+  loading
+});
+
+export const removeSaved = id => ({
+  type: 'REMOVE_SAVED',
+  id
+});
+
+export const addSaved = savedBoost => ({
+  type: 'ADD_SAVED',
+  savedBoost
+});
