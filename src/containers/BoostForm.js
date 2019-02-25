76571bb7cf9ee37044931fcf2e4ef3a9bd7f358a
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-25 13:13:10 -0700

    Add BoostForm container

--- src/containers/BoostForm.js
+++ src/containers/BoostForm.js
@@ -1,13 +1,87 @@
-import React, { Component } from 'react';
-
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+var shortid = require('shortid');
 class BoostForm extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      senderNum: '',
+      recipientNum: '',
+      boostMessage: ''
+    };
   }
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSave = e => {
+    const { dogImgSrc } = this.props;
+    const { senderNum, recipientNum, boostMessage } = this.state;
+    let boost = {
+      id: shortid.generate(),
+      senderNum,
+      recipientNum,
+      boostMessage,
+      dogImgSrc
+    };
+  };
+
   render() {
-    return <div>boost form</div>;
+    const { dogImgSrc } = this.props;
+    const { senderNum, recipientNum, boostMessage } = this.state;
+    const url = 'https://random.dog/' + dogImgSrc;
+    return (
+      <form onSubmit={this.handleSubmit} className="BoostForm">
+        <div className="div-flex">
+          <div className="input-div">
+            <label>
+              To:
+              <input
+                onChange={this.handleChange}
+                name="senderNum"
+                type="number"
+                placeholder="855-546-1234"
+                value={senderNum}
+              />
+            </label>
+            <label>
+              From:
+              <input
+                onChange={this.handleChange}
+                name="recipientNum"
+                type="number"
+                placeholder="765-765-2343"
+                value={recipientNum}
+              />
+            </label>
+          </div>
+          <img src={url} alt="animal" />
+        </div>
+        <textarea
+          onChange={this.handleChange}
+          name="boostMessage"
+          placeholder="Write your boost here"
+          value={boostMessage}
+        />
+        <button name="save" onClick={this.handleSave}>
+          Save
+        </button>
+        <button name="send" type="submit">
+          Send
+        </button>
+      </form>
+    );
   }
 }
 
-export default BoostForm;
+export const mapStateToProps = state => ({
+  dogImgSrc: state.dogImgSrc
+});
+
+export default connect(mapStateToProps)(BoostForm);
