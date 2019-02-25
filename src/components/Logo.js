af425211c572550c2b99ac2a9a416ca9c66d8d9a
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-24 20:20:58 -0700

    Add Link to Logo to route to '/'

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from '../images/betheboost.png';
+import { Link } from 'react-router-dom';
 
 export const Logo = () => {
   return (
     <footer className="Logo-footer">
-      <img src={logo} alt="be the boost logo" height="200px" />
+      <Link to="/">
+        <img src={logo} alt="be the boost logo" height="200px" />
+      </Link>
     </footer>
   );
 };
