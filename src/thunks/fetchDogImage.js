4876c24282062bce52f94791d9d2815387acf360
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-27 00:23:15 -0700

    Fix fetchDogImage thunk

--- src/thunks/fetchDogImage.js
+++ src/thunks/fetchDogImage.js
@@ -7,12 +7,15 @@ export const fetchDogImage = () => {
       dispatch(setLoading(true));
       const url = 'https://random.dog/doggos';
       const res = await fetch(url);
+      if (!res.ok) {
+        throw Error(res.statusText);
+      }
+      dispatch(setLoading(false));
       const dogImages = await res.json();
       const randomImage = dogImages[randomIndex(dogImages.length - 1)];
       dispatch(setDogImage(randomImage));
     } catch (error) {
-      console.log(error);
+      dispatch(setError('There was an error, please refresh'));
     }
-    dispatch(setLoading(false));
   };
 };
