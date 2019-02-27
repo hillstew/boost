dec9abf0d305526d846ae48c10cfef181f27890a
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-27 07:42:04 -0700

    Add about text and image

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export const About = () => {
-  return <p>a lot of stuff of about be the boost</p>;
+  const url = require('../images/good-things.jpg');
+  return (
+    <div className="About">
+      <h1>About Be the Boost </h1>
+      <p>
+        Sometimes in life, you find it hard to encourage your friend going through a hard time, of having a bad day.
+        Boost is a way to send your friend a picture of a dog (cats coming soon!) and message.
+      </p>
+      <img src={url} alt="good news is coming" />
+    </div>
+  );
 };
