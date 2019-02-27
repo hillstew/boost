e97af4c58eee90ff8934ad10d322b41cf61a9646
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-27 00:21:02 -0700

    Add BoostCard tests

--- src/__tests__/BoostCard.js
+++ src/__tests__/BoostCard.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { BoostCard } from '../components/BoostCard';
 import { shallow } from 'enzyme';
+import { fetchDogImage } from '../thunks/fetchDogImage';
+jest.mock('../thunks/fetchDogImage');
 
 describe('BoostCard', () => {
+  let mockImg = 'hfdskfj.jpg';
+  let mockFetchDog;
+  let wrapper;
+
+  beforeEach(() => {
+    mockImg = 'hfdskfj.jpg';
+    mockFetchDog = jest.fn();
+    wrapper = shallow(<BoostCard img={mockImg} fetchDogImage={mockFetchDog} />);
+  });
+
   it('should match the snapshot', () => {
-    let wrapper = shallow(<BoostCard />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call fetchDogImage onClick', () => {
+    let wrapper = shallow(<BoostCard img={mockImg} fetchDogImage={mockFetchDog} />);
+    console.log(wrapper.instance());
+    // const spy = jest.spyOn(wrapper.instance(), 'fetchDogImage');
+    // wrapper.forceUpdate();
+    wrapper.find('BoostCard-button').simulate('click');
+    expect(wrapper.instance().props.fetchDogImage).toHaveBeenCalled();
   });
 });
