488b78a549d9bce35e8a7995c9f57d3c517da2e0
Hillary Stewart <hillarystewart4427@gmail.com>
2019-03-19 13:06:05 -0600

    Update SavedBoosts

--- src/containers/SavedBoosts.js
+++ src/containers/SavedBoosts.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { removeSaved } from '../actions';
+import { postData } from '../api';
 import PropTypes from 'prop-types';
 
 export class SavedBoosts extends Component {
   renderSavedBoosts = () => {
     const { removeSaved, saved } = this.props;
-    const url = 'https://random.dog/';
     return saved.map(boost => {
       return (
-        <div key={boost.id} className="SavedBoosts-boost">
+        <div className="SavedBoosts-boost" key={boost.id}>
           <div className="SavedBoosts-flex">
             <div className="div">
-              <p>TO: {boost.senderNum}</p>
-              <p>FROM: {boost.recipientNum}</p>
+              <p>TO: {boost.to}</p>
+              <p>FROM: {boost.senderName}</p>
             </div>
-            <img src={url + boost.dogImgSrc} alt="animal" />
+            <img src={boost.mediaUrl} alt="animal" />
           </div>
-          <p className="boost-msg">Your boost messsage: {boost.boostMessage}</p>
+          <p className="boost-msg">{boost.body}</p>
+          <button name="send" onClick={() => postData(boost)} className="remove-button">
+            Send
+          </button>
           <button name="delete" onClick={() => removeSaved(boost.id)} className="remove-button">
             Delete
           </button>
