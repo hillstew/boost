9daa61b96d2ddfaecd8fa0aa1c967f859b3f868d
Hillary Stewart <hillarystewart4427@gmail.com>
2019-03-21 14:58:08 -0600

    Update about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 export const About = () => {
-  const url = require('../images/good-things.jpg');
   return (
     <div className="About">
-      <h1>About Be the Boost </h1>
+      <h1>About</h1>
       <p>
-        Sometimes in life, you find it hard to encourage your friend going through a hard time, of having a bad day.
-        Boost is a way to send your friend a picture of a dog (cats coming soon!) and message.
+        Sometimes in life, you find it hard to encourage your friend going through a hard time, or having a bad day.
       </p>
-      <img src={url} alt="good news is coming" />
+      <br />
+      <p> Boost is a way to send your friend a picture of a furry friend and message.</p>
     </div>
   );
 };
