302663c62b1cee0d61dadce392ee6e9af0a62084
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-02 14:07:29 -0600

    Create provider variable and update ReactDOM render

--- src/index.js
+++ src/index.js
@@ -15,14 +15,15 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(thunk))
 );
 
-ReactDOM.render(
+const provider = (
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
+
+ReactDOM.render(provider, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
