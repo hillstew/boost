2aec4956cf9e18fee8fda9473f4ae15e5081b239
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-02 20:06:15 -0600

    Add media queries to Nav and move Logo into Nav

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import logo from '../images/betheboost.png';
-import { Link } from 'react-router-dom';
 
 export const Logo = () => {
-  return (
-    <footer className="Logo-footer">
-      <Link to="/">
-        <img src={logo} alt="be the boost logo" height="200px" />
-      </Link>
-    </footer>
-  );
+  return <img src={logo} alt='be the boost logo' className='logo-img' />;
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Logo } from '../components/Logo';
 
 export const Nav = () => {
   return (
-    <nav className="Nav">
-      <NavLink to="/saved">Saved</NavLink>
-      <hr />
-      <NavLink to="/sent">Sent</NavLink>
-      <hr />
-      <NavLink to="/about">About</NavLink>
-      <hr />
+    <nav className='Nav'>
+      <div className='App-nav-logo-div'>
+        <Logo />
+        <div classname='App-links'>
+          <NavLink to='/saved' className='Nav-a'>
+            Saved
+          </NavLink>
+          <hr />
+          <NavLink to='/sent' className='Nav-a'>
+            Sent
+          </NavLink>
+          <hr />
+          <NavLink to='/about' className='Nav-a'>
+            About
+          </NavLink>
+          <hr />
+        </div>
+      </div>
     </nav>
   );
 };
